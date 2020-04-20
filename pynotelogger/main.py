77d336f895bcbe913d09efd574d1365adf167c35
sroehl <stevenroehl@gmail.com>
2020-04-20 08:03:22 -0500

    Adding loading args and cleanup

--- pynotelogger/main.py
+++ pynotelogger/main.py
@@ -1,7 +1,14 @@
 from notebook import Notebook
 from note import Note
+import argparse
+
+def load_args():
+    parser = argparse.ArgumentParser(description='Python Note Logger')
+    parser.add_argument()
+
 
 if __name__ == '__main__':
+    load_args()
     notebook = Notebook()
     test_note = Note('Books I have read', '2019 Books', 'Books', ['read', 'books', '2019'])
     notebook.add_note(test_note)
